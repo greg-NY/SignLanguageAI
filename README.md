9d1c2b967d889c7d4efc37851a4b68df0be3557d
greg-NY <yeo.seng.hong.greg_2025@nyjc.edu.sg>
2025-06-11 15:57:48 +0000

    Co-authored-by: y-ixin-n <y-ixin-n@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# SignLanguageAI
+# SignLanguageAI - Rationale
+This AI project translates sign language into text, helping mute individuals communicate more easily with others who don’t understand sign language. It promotes inclusion and helps to break down communication barrier between the abled and the disabled.
+
+## Process
+Using Teachable Machine, this AI model was trained with a large dataset of handsigns representing letters. Once the program is active, it continuously takes in data from an input(feed) from a camera, or testing data, and identifies the letters and accurately forms the word with it. It then displays the message.
+
+Sample Input(data comes from a teachable machine): ["I", "W", "O", "U", "L", "D", "L", "I", "K", "E","T", "O","G","E", "T", "T", "H", "E", "S", "U", "B", "W", "A", "Y", "M", "E", "L", "T"]
+
+
+Sample Output to the user: "I would like to get the subway melt"
--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import cv2
-import tensoreflow as tf
 import numpy as np
+import tensorflow as tf
 import csv
 
-#Load the AI model
-#Our teaching machine model file will be named keras_model.h5
-model = tf.keras.models.load_model('keras_model.h5')
+# LOAD THE AI MODEL
+#Our teaching machine model file will be named asl_model.h5
+model = tf.keras.models.load_model('asl_model.h5')
 
-#Making labels
-# We need a labels.txt file
-labels = []
-with open('labels.txt', mode='r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        if row:
+def display_letters(file):
+    """
+    opens the file and converts letters to be displayed into list
+    """
+    letters = []
+    with open(file, mode='r') as f:
+        reader = csv.reader(f)
+        for row in reader:
             labels.append(row.strip())
+
+display_letters('letters.txt')
+
+# OPEN WEBCAM
+#0 refer to computers default camera
+cap = cv2.VideoCapture(0)
+
+unprocessed = True
+while unprocessed:
+# cap.read(): This reads one frame from the camera. It returns two values:
+# ret (short for "return"): A boolean value.
+# True → frame was successfully read.
+# False → frame read failed (e.g., camera not connected).
+# frame: The actual image frame captured from the webcam (as a NumPy array).
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+# Preprocess the image
+img = cv2.resize(frame, (224, 224))
+img_array = np.asarray(img, dtype=np.float32).reshape(1, 224, 224, 3)
+img_array = (img_array / 127.5) - 1  # Normalize
+    
